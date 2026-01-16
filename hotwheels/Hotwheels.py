8bdc9ce90971fae2b11b567b14fc3d1b1a34eecc
vijeshvasudevan08-ai <vijeshvasudevan08@gmail.com>
2026-01-17 01:31:57 +0530

    Update Hotwheels.py

--- hotwheels/Hotwheels.py
+++ hotwheels/Hotwheels.py
@@ -47,4 +47,6 @@ def select():
  except Exception as e:
      return render_template("Hotwheelsindex.html", error=str(e))
 if __name__ == "__main__":
- app.run(host="0.0.0.0", port=5000, debug=True)
+ port = int(os.environ.get("PORT",10000))
+ app.run(host="0.0.0.0", port=port)
+
